8697cc8b5ee633e1684b62180e9a4e519aa2bfd2
shailendrasharma28 <shailendr2003@gmail.com>
2025-10-07 16:16:45 +0530

    form implimentation with 302 redirection after submit

--- app.js
+++ app.js
@@ -2,23 +2,27 @@ const http = require("http")
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-    console.log("Server is created!")
     res.setHeader('Content-type', 'text/html')
-    if (req.url === "/home") {
-      res.end("<h1>Welcome home</h1>");
+    if (req.url === "/") {
+      res.end(
+        `
+          <form method="POST" action="/message">
+          <label>Username</label>
+          <input type="text" name="username" />
+          <button type="submit">Submit</button>
+          </form>
+        `
+      );
     } else {
-      if (req.url === "/home") {
-        res.end("<h1>Welcome home</h1>");
-      } else if (req.url === "/about"){
-        res.end("<h1>Welcome to About Us</h1>");
-      } else if (req.url === "/node"){
-        res.end("<h1>Welcome to my Node Js project</h1>");
-      } else {
-        res.end("<h1>Page Not Found</h1>");
+      if (req.url === "/message") {
+        res.statusCode = 302;
+        res.end(`
+          <h1>Landed To message page!</h1>
+        `);
       }
     }
 })
 
 server.listen(port, () => {
-    console.log("Server listing on port: 3000")
+    console.log("Server listing on port: 8080")
 })
