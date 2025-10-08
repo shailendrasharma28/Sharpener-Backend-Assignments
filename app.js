23b1db40c7df93e5c3e3d11c9991a6bb3a5666c5
shailendrasharma28 <shailendr2003@gmail.com>
2025-10-08 16:41:17 +0530

    routes added and handled wildcard routes with error

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-const port = 3000;
+const port = 4000;
 const express = require('express');
 const app = express();
 
-// Middleware checking authentication
-app.use('/welcome',(req, res, next) => {
-  console.log("Auth Middleware called!");
-  const user = "Guest"
-  req.user = user;
-  next();
-});
 
 // homepage route
 app.get("/", (req, res) => {
-  res.send(`Welcome to the homepage!`)
+  res.send(`<h1>Welcome to the homepage!</h1>`)
 })
 
 // All recommended routes
-app.get("/orders", (req, res) => {
-  res.send(`<h1>Here is the list of all orders</h1>`)
+app.get("/products", (req, res) => {
+  res.send(`<h1>Here is the list of all products</h1>`)
 })
-app.post("/orders", (req, res) => {
-  res.send(`<h1>A new order has been created.</h1>`)
+app.post("/products", (req, res) => {
+  res.send(`<h1>A new product has been added.</h1>`)
 })
-app.get("/users", (req, res) => {
-  res.send(`<h1>Here is the list of all users.</h1>`)
+app.get("/categories", (req, res) => {
+  res.send(`<h1>Here is the list of all categories.</h1>`)
 })
-app.post("/users", (req, res) => {
-  res.send(`<h1>A new user has been added.</h1>`)
+app.post("/categories", (req, res) => {
+  res.send(`<h1>A new category has been created.</h1>`)
 })
 
+// Wildcard Route handling with 404 not found errror page
+app.use((req, res) => {
+   res.status(404).type('html').send('<h1>404 - Page Not Found</h1>');
+});
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
