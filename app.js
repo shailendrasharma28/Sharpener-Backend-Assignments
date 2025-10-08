6bb1c16278c2de2838863e2935ad8462fdd218bc
shailendrasharma28 <shailendr2003@gmail.com>
2025-10-08 15:49:25 +0530

    Custom Middleware created and set routing for the middleware

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const port = 3000;
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  res.send("<h1>Hello World!</h1>");
+app.use('/welcome',(req, res, next) => {
+  console.log("Auth Middleware called!");
+  const user = "Guest"
+  req.user = user;
   next();
+});
+
+app.get("/", (req, res) => {
+  res.send(`Welcome to the homepage!`)
+})
+app.get("/welcome", (req, res) => {
+  const user = req.user;
+  res.send(`<h1>Welcome, ${user}!</h1>`)
 })
 
 app.listen(port, () => {
