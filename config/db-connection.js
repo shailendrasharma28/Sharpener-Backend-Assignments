5fd45a3c6eba90732627bcb8da60deda12df1602
shailendrasharma28 <shailendr2003@gmail.com>
2025-10-11 20:59:30 +0530

    CRUD Operations using sequelize

--- config/db-connection.js
+++ config/db-connection.js
@@ -2,7 +2,8 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("mydb", "root", "", {
   host: "localhost",
-  dialect: "mysql"
+  dialect: "mysql",
+  logging: false
 })
 
 const testConnection = async () => {
--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,84 +1,68 @@
 const db = require("../config/db-connection");
+const Student = require("../models/studentModel");
 
 const userController = {
-    addUser: (req, res) => {
+    addUser: async (req, res) => {
         const {name, email, age} = req.body;
-        const addQuery = `INSERT INTO students (name, email, age) VALUES (?, ?, ?)`;
-
-        db.execute(addQuery, [name, email, age], (err) => {
-            if(err) {
-                console.log(err);
-                res.status(500).send("Error in adding student!");
-                db.end();
-                return;
-            }
-            res.status(201).send("Student details added successfully!");
-            console.log(`Student details: ${name}, ${email} ${age}`);
-        });
+        try {
+            const student = await Student.create({name:name, email:email, age:age})
+            console.log("Student added successfully!");
+            console.log(student);
+            
+            res.status(200).send('New Student', student);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send("Error creating student record", error);
+        }
     },
 
-    getUsers: (req, res) => {
-        const getQuery = `SELECT * FROM students`;
-
-        db.execute(getQuery, (err, results) => {
-            if(err) {
-                console.log(err);
-                res.status(500).send("Error fetching student!");
-                db.end();
-                return;
-            }
-            res.status(201).send("Student details fetched successfully!");
-            console.log(`Student details:`, results);
-        });
+    getUsers: async (req, res) => {
+       try {
+            const students = await Student.findAll()
+            console.log("All Student details fetched successfully!", students);
+            res.status(200).send('All Students', students);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send("Error fetching student recorda", error);
+        }
     },
 
-    getUserById: (req, res) => {
+    getUserById: async (req, res) => {
         const {id} = req.params;
-        const getQuery = `SELECT * FROM students WHERE id = ?`;
-
-        db.execute(getQuery, [id], (err, results) => {
-            if(err) {
-                console.log(err);
-                res.status(500).send("Error fetching student!");
-                db.end();
-                return;
-            }
-            res.status(201).send("Student details fetched successfully!");
-            console.log(`Student details:`, results);
-        });
+        try {
+            const student = await Student.findByPk(id)
+            console.log("Student details fetched successfully!", student.dataValues);
+            res.status(200).send('Student', student.dataValues);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send("Error fetching student record", error);
+        }
     },
 
-    updateUser: (req, res) => {
+    updateUser: async (req, res) => {
         const {id} = req.params;
         const {name, email, age} = req.body;
-        const updateQuery = `UPDATE students SET name = ?, email = ?, age = ? WHERE id = ?`;
-
-        db.execute(updateQuery, [name, email, age, id], (err) => {
-            if(err) {
-                console.log(err);
-                res.status(500).send("Error in updating student!");
-                db.end();
-                return;
-            }
-            res.status(201).send("Student details updated successfully!");
-            console.log(`Student details: ${name} & ${email}`);
-        });
+        try {
+            const student = await Student.update({name:name, email:email, age:age}, {where: {id: id}});
+            const updated = await Student.findByPk(id)
+            console.log("Student details updated successfully!", updated);
+            res.status(200).send('Updated Student', updated);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send("Error updating student record", error);
+        }
     },
 
-    deleteUser: (req, res) => {
+    deleteUser: async (req, res) => {
         const {id} = req.params;
-        const updateQuery = `DELETE FROM students WHERE id = ?`;
-
-        db.execute(updateQuery, [id], (err) => {
-            if(err) {
-                console.log(err);
-                res.status(500).send("Error in deleting student!");
-                db.end();
-                return;
-            }
-            res.status(201).send("Student details deleted successfully!");
-            console.log(`Student details deleted successdully!`);
-        });
+        try {
+            const student = await Student.destroy({where: {id}})
+            console.log("Student details deleted successfully!");
+            res.status(200).send("Deleted!");
+        } catch (error) {
+            console.log(error);
+            res.status(500).send("Error deleting student record", error);
+        }
     },
 };
 
